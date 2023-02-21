3902707aefb946cf1030f02f58d486f2224a0036
hkrainko <>
2023-02-21 22:04:58 +0800

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Header from "./app/header/Header";
-import {Button} from "@mui/material";
+import {
+    Avatar,
+    Button,
+    Card, CardActions,
+    CardContent,
+    CardHeader,
+    CardMedia,
+    Container,
+    IconButton,
+    Typography
+} from "@mui/material";
 import AuthView from "./app/auth/AuthView";
 
 function App() {
@@ -12,29 +22,47 @@ function App() {
       <BrowserRouter>
           <Header/>
 
-          <Routes>
-              <Route path="/auth" element={<AuthView/>}/>
+          <Container maxWidth="sm">
 
-              {/*<Route path="*" element={<AuthView/>}/>*/}
-          </Routes>
-          <Button variant="contained">Contained</Button>
+              <h2>2555 USD</h2>
+              <h2>= 2555 USDT</h2>
+
+              <h2>3332</h2>
+              <h2>3332</h2>
+
+              <Card>
+                  <CardHeader>
+                        <h2>3332</h2>
+                  </CardHeader>
+                  <CardMedia
+                      component="img"
+                      height="194"
+                      image="/static/images/cards/paella.jpg"
+                      alt="Paella dish"
+                  />
+                  <CardContent>
+                      <Typography variant="body2" color="text.secondary">
+                          This impressive paella is a perfect party dish and a fun meal to cook
+                          together with your guests. Add 1 cup of frozen peas along with the mussels,
+                          if you like.
+                      </Typography>
+                  </CardContent>
+              </Card>
+          </Container>
+
+
+
+          {/*<Routes>*/}
+          {/*    <Route path="/auth" element={<AuthView/>}/>*/}
+
+          {/*    /!*<Route path="*" element={<AuthView/>}/>*!/*/}
+          {/*</Routes>*/}
+
+
+
+          {/*<Button variant="contained">Contained</Button>*/}
 
       </BrowserRouter>
-
-      {/*<header className="App-header">*/}
-      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.tsx</code> and save to reload.*/}
-      {/*  </p>*/}
-      {/*  <a*/}
-      {/*    className="App-link"*/}
-      {/*    href="https://reactjs.org"*/}
-      {/*    target="_blank"*/}
-      {/*    rel="noopener noreferrer"*/}
-      {/*  >*/}
-      {/*    Learn React*/}
-      {/*  </a>*/}
-      {/*</header>*/}
     </div>
   );
 }
