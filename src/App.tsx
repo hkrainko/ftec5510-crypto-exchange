87eb8a9fc079d5d3f4c9b66883f3a78caa09b657
hkrainko <>
2023-03-07 21:38:53 +0800

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,8 @@ import AuthView from "./app/auth/AuthView";
 import OrderRepo from "./domain/interfaces/repositories/order.repo";
 import OrderUseCase from "./domain/interfaces/usecases/order.usecase";
 import DefaultOrderUsecase from "./domain/usecases/default.order.usecase";
+import Order from "./domain/entities/order/order";
+import OrderResult from "./domain/entities/order/order-result";
 
 const orderUseCase: OrderUseCase = new DefaultOrderUsecase()
 
@@ -26,22 +28,29 @@ function App() {
     const queryParams = new URLSearchParams(window.location.search)
     const redirectUrl = queryParams.get('redirectUrl')
 
+    const [getOrder, setOrder] = React.useState<Order | null>(null)
+    const [getOrderResult, setOrderResult] = React.useState<OrderResult | null>(null)
+
     useEffect(() => {
         orderUseCase.createOrder({
             currency: "USDT",
             expireTime: 60000,
-            orderAmount: 100,
+            orderAmount: 0.0000001,
             supportPayCurrency: [
                 "USDT",
             ]
         }).then((order) => {
-            console.log(`createOrder success:${order}`)
+            console.log(`createOrder success:${JSON.stringify(order)}`)
+            setOrder(order)
             return orderUseCase.queryOrder({
                 prepayId: order.prepayId
             })
         }).then((orderResult) => {
-            console.log(`queryOrder success:${orderResult}`)
+            console.log(`queryOrder success:${JSON.stringify(orderResult)}`)
+            setOrderResult(orderResult)
+            // Terminate the payment process with success code
         }).catch((e) => {
+            // Terminate the payment process with fail code
             console.log(e)
         });
     }, [])
@@ -58,6 +67,8 @@ function App() {
             <BrowserRouter>
                 <Header/>
 
+
+                if (getOrder) {
                 <Box mt={16}>
                     <Container maxWidth="xs">
                         <h2>2555 USD</h2>
@@ -86,21 +97,25 @@ function App() {
                                     subheader="Scan this QRCode in the Binance APP"
                                 />
                             </Box>
-                            <Box p={8} mt={-8} mb={-8}>
+                            <Box p={8} mt={-8} mb={-11}>
                                 <CardMedia
                                     component="img"
-                                    image="qrcode.png"
+                                    image={getOrder?.qrcodeLink}
                                     alt="Paella dish"
                                 />
                             </Box>
+                            <Typography variant="body2" color="text.secondary">
+                                {getOrder?.prepayId}
+                            </Typography>
                             <CardContent>
-                                <Typography variant="body2" color="text.secondary">
+                                <Typography variant="body1" color="text.secondary">
                                     Time remaining 09:12
                                 </Typography>
                             </CardContent>
                         </Card>
                     </Container>
                 </Box>
+            }
 
 
                 <Box mt={2}>
@@ -110,16 +125,6 @@ function App() {
                         <Typography variant="body1" color="text.secondary">Cancel</Typography>
                     </Button>
                 </Box>
-
-
-                {/*<Routes>*/}
-                {/*    <Route path="/auth" element={<AuthView/>}/>*/}
-
-                {/*    /!*<Route path="*" element={<AuthView/>}/>*!/*/}
-                {/*</Routes>*/}
-
-
-                {/*<Button variant="contained">Contained</Button>*/}
 
             </BrowserRouter>
         </div>
