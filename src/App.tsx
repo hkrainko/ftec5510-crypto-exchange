29d58b36b79f9b69f7dcf720f5b8d494537ca67c
gary <>
2023-03-13 20:35:20 +0800

    Rebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Header from "./app/header/Header";
